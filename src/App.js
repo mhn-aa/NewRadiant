52e9c77b73c818d6c3da556e04f025eedfc18969
mhn-aa <mahanmirjamali@gmail.com>
2021-08-31 22:05:41 +1000

    learning git

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import PregnantWomanIcon from "@material-ui/icons/PregnantWoman";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import StopIcon from "@material-ui/icons/Stop";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="Main_Nav_bar">
-      
-      
+    <div>
+      <div className="header">
         <div className="left1">
           <h1>R</h1>
         </div>
         <div className="left2">
           <h3>Menu</h3>
         </div>
-
         <div className="left3">
           <h1>P</h1>
         </div>
@@ -21,27 +23,28 @@ function App() {
             <h1>RADIANT</h1>
           </a>
         </div>
-        <div className="right3"></div>
-        <div className="right2"></div>
+        <div className="right3">
+          <VisibilityIcon className="eye" />
+        </div>
+        <div className="right2">
+          <h1>ET</h1>
+        </div>
         <div className="right1"></div>
         <div className="generosity0"> </div>
         <div className="generosity-1"> </div>
         <div className="generosity-2"> </div>
         <div className="generosity-3">
-            <button>Log in</button>
+          <button>Log in</button>
         </div>
-        
-      
+      </div>
 
+      <div className="body">
+        <p>body</p>
+      </div>
+      <div className="div">
+        <p>footer</p>
+      </div>
     </div>
-
-
-
-
-
-
-
-
   );
 }
 
