51adbe131eda3fecb0591fbbece7e19a6bef1393
mhn-aa <mahanmirjamali@gmail.com>
2021-08-31 20:03:58 +1000

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main_Nav_bar">
+      
+      
+        <div className="left1">
+          <h1>R</h1>
+        </div>
+        <div className="left2">
+          <h3>Menu</h3>
+        </div>
+
+        <div className="left3">
+          <h1>P</h1>
+        </div>
+        <div className="center1">
+          <a class="Radiant" href="./index.html">
+            <h1>RADIANT</h1>
+          </a>
+        </div>
+        <div className="right3"></div>
+        <div className="right2"></div>
+        <div className="right1"></div>
+        <div className="generosity0"> </div>
+        <div className="generosity-1"> </div>
+        <div className="generosity-2"> </div>
+        <div className="generosity-3">
+            <button>Log in</button>
+        </div>
+        
+      
+
     </div>
+
+
+
+
+
+
+
+
   );
 }
 
