ba9be19b2c963186476e989937d2ba0ea990f335
mhn-aa <mahanmirjamali@gmail.com>
2021-09-04 12:48:15 +1000

    RadiantLogin

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ function App() {
     email: "admin@admin.com",
     password: "admin123",
   };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+  };
+
+  const Logout = () => {
+    console.log("Logout");
+  };
+
   return (
     <div>
       <div className="header">
