55bc939b3545cc675a4305abb28b9d0a90cf3817
mhn-aa <mahanmirjamali@gmail.com>
2021-10-06 16:04:17 +1100

    GreenLinks

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,48 +8,19 @@ const Header = () => {
 
   return (
     <div className="header">
-      <div className="left1">
-        <svg width="90" height="90">
-          <circle
-            cx="45"
-            cy="45"
-            r="30"
-            stroke="Royalblue"
-            stroke-width="0"
-            fill="orange"
-          />
-        </svg>
-        {/* <svg width="100" height="100" className="Cross"></svg> */}
-      </div>
-      <div className="left2">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-      </div>
-      <div className="left3">
-        <Link to="/">
-          <svg
-            width="80"
-            height="80"
-            fill="gold"
-            stroke="black"
-            stroke-width="0"
-          >
-            <rect x="19" y="19" rx="6" ry="6" width="42" height="42" />
-          </svg>
-        </Link>
-      </div>
+      <div className="left1"></div>
+      <div className="left3"></div>
       <div className="center1">
         <Link to="/">
           <h1 className="Name">EXAMPLE</h1>
         </Link>
       </div>
-      <div className="right3">{/* <VisibilityIcon className="eye" /> */}</div>
-      <div className="right2">{/* <h1>ET</h1> */}</div>
-      <div className="right1">{/* <h4>Alimo</h4> */}</div>
-      <div className="generosity-0">{/* <h1>F1</h1> */}</div>
-      <div className="generosity-1">{/* <h1>F2</h1> */}</div>
-      <div className="generosity-2">{/* <h1>F3</h1> */}</div>
+      <div className="right3"></div>
+      <div className="right2"></div>
+      <div className="right1"></div>
+      <div className="generosity-0"></div>
+      <div className="generosity-1"></div>
+      <div className="generosity-2"></div>
       <div className="generosity-3">
         <Link to="/login">
           <button onClick={Login}>Log in</button>
--- src/Components/Links.js
+++ src/Components/Links.js
@@ -4,25 +4,60 @@ const Links = () => {
   return (
     <div className="Mission">
       <h3 className="r">"Radiant is a Website for Conciousness Expansion."</h3>
-      <a href="./People.js">People</a>
-      <a href="./Books.js">Books</a>
-      <a href="./Art.js">Art </a>
-      <a href="./Drugs.js">Drugs </a>
-      <a href="./Politics.js">Politics </a>
-      <a href="./Culture.js">Culture </a>
-      <a href=".Language.js">Language </a>
-      <a href="./Sexuality.js">Sexuality </a>
-      <a href="./Sprituality.js">Sprituality </a>
-      <a href="./Communication.js">Communication </a>
-      <a href="./Archeology.js">Archeology </a>
-      <a href="./Technology.js"> Technology </a>
-      <a href="./Astrology.js">Astrology</a>
-      <a href="./Philosophy.js">Philosophy</a>
-      <a href="./History.js">History</a>
-      <a href="./Coding.js">Coding</a>
-      <a href="./Geometry.js">Geometry</a>
-      <a href="./Geometry.js">Psychology</a>
-      <a href="./Geometry.js">Gastronomy</a>
+      <Link to="/">
+        <p>People</p>
+      </Link>
+      <Link to="/">
+        <p>Books</p>
+      </Link>
+      <Link to="/">
+        <p>Art</p>
+      </Link>
+      <Link to="/">
+        <p>Drugs</p>
+      </Link>
+      <Link to="/">
+        <p>Politics</p>
+      </Link>
+      <Link to="/">
+        <p>Culture</p>
+      </Link>
+      <Link to="/">
+        <p>Language</p>
+      </Link>
+      <Link to="/">
+        <p>Sprituality</p>
+      </Link>
+      <Link to="/">
+        <p>Communication</p>
+      </Link>
+      <Link to="/">
+        <p>Archeology</p>
+      </Link>
+      <Link to="/">
+        <p>Technology</p>
+      </Link>
+      <Link to="/">
+        <p>Astrology</p>
+      </Link>
+      <Link to="/">
+        <p>Philosophy</p>
+      </Link>
+      <Link to="/">
+        <p>History</p>
+      </Link>
+      <Link to="/">
+        <p>Coding</p>
+      </Link>
+      <Link to="/">
+        <p>Geometry</p>
+      </Link>
+      <Link to="/">
+        <p>Psychology</p>
+      </Link>
+      <Link to="/">
+        <p>Gastronomy</p>
+      </Link>
       <a href="./Geometry.js">About Us</a>
     </div>
   );
