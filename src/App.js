6a25aa4b1f58463f62686bc2620be0bf58398b5d
mhn-aa <mahanmirjamali@gmail.com>
2021-10-17 19:41:59 +1100

    Complicated

--- src/App.js
+++ src/App.js
@@ -25,21 +25,7 @@ function App() {
         </Route>
         <Route path="/" exact>
           <div className="body">
-            <p>
-              I was happy to have that excess coffee. That Addict type thing.
-            </p>
-            <p>Metaphore</p>
-            <p>Time travel</p>
-            <p>forward scape</p>
-            <p>siren</p>
-            <p>Generation of the perfect tool</p>
-            <p>self transcendance</p>
-            <p>our own method</p>
-            <p>Money</p>
-            <p>Illuminati</p>
-            <p>Meditation</p>
-            <p>Working Out (Yoga)</p>
-            <p>The Text-Book of Astrology</p>
+            <p>I am just an ordinary person</p>
           </div>
 
           <div className="footer"></div>
