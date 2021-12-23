b20204ed23bd4077584eadc42acd3ddc27984af9
mhn-aa <mahanmirjamali@gmail.com>
2021-12-23 22:20:48 +1100

    NH23Dec

---
+++ src/Components/People.js
@@ -0,0 +1,30 @@
+const People = () => {
+  return (
+    <div>
+      <h2>People</h2>
+      <p>Terence McKenna</p>
+      <a href="https://www.youtube.com/watch?v=-3W6zGY5yt4" target="_blank">
+        DMT is Everything
+      </a>
+      <p>
+        {" "}
+        As far as DMT is concerned, Ayahusca is driven by DMT, What made me go
+        to the amazon was I first encountered DMT in the underground in berkeley
+        in 1967.{" "}
+      </p>
+    </div>
+  );
+};
+
+export default People;
+
+// import React from "react";
+
+// function People() {
+//   const People = {
+//     People1: "",
+//     People2: "",
+//     People3: "",
+//   };
+//   return <div></div>;
+// }
